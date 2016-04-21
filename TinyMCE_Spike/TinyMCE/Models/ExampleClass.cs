7adf008d3cb9c8f696d2503f1bdb051ac3f4adc1
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-21 10:12:12 -0400

    TinyMCE spike!

---
+++ TinyMCE_Spike/TinyMCE/Models/ExampleClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinyMCE.Models
+{
+    public class ExampleClass
+    {
+        // This attribute allows your HTML Content to be sent up
+        [AllowHtml]
+        public string HtmlContent { get; set; }
+        
+
+        public ExampleClass()
+        {
+
+        }
+    }
+}
