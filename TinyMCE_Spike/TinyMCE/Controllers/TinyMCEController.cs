5e84048ca921ef179331fc50762edaa922a44a95
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-23 12:01:01 -0400

    KKJC-8 spike for TinyMCE is done.

--- TinyMCE_Spike/TinyMCE/Controllers/TinyMCEController.cs
+++ TinyMCE_Spike/TinyMCE/Controllers/TinyMCEController.cs
@@ -9,18 +9,26 @@ namespace TinyMCE.Controllers
 {
     public class TinyMCEController : Controller
     {
-    
-       // An action to display your TinyMCE editor
-       public ActionResult Index()
-       {
-                return View();
-       }
+
+        private static List<ExampleClass> _repo = new List<ExampleClass>();
+
+        public ActionResult Index()
+        {
+            return View(_repo);
+        }
+
+        // An action to display your TinyMCE editor
+        public ActionResult Add()
+        {
+            return View("Add", new ExampleClass());
+        }
 
         // An action that will accept your Html Content
-       [HttpPost]
-       public ActionResult Index(ExampleClass model)
-       {
-            return View(model);
-       }
+        [HttpPost]
+        public ActionResult Add(ExampleClass model)
+        {
+            _repo.Add(model);
+            return RedirectToAction("Index");
+        }
     }
 }
