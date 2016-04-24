8610538f22295aa443b108d91a5810a4b33c3f84
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-23 22:33:12 -0400

    KKJC-14 vertical slices for post

--- AnatoKnightStudios/AnatoKnightStudios.Data/Repos/BlogRepo.cs
+++ AnatoKnightStudios/AnatoKnightStudios.Data/Repos/BlogRepo.cs
@@ -1,18 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AnatoKnightStudios.UI.Models;
+using Dapper;
 
 namespace AnatoKnightStudios.Data.Repos
 {
     public class BlogRepo
     {
+        private string conn;
+
+        public BlogRepo()
+        {
+            conn = ConfigurationManager.ConnectionStrings["AnatoKnightStudios"].ConnectionString;
+        }
+
         public List<Post> GetAllPosts()
         {
-            var posts = new List<Post>();
-            return posts;
+            using (var _cn = new SqlConnection(conn))
+            {
+                List<Post> posts = new List<Post>();
+
+                posts = _cn.Query<Post>("SELECT * FROM Post ").ToList();
+                return posts;
+            }
+        }
+
+        public Post GetPostById(int id)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("ID", id);
+                var post = _cn.Query<Post>("SELECT * " +
+                                           "FROM Post " +
+                                           "WHERE PostId = @ID ", parameters).FirstOrDefault();
+                return post;
+            }
+        }
+
+        public Post GetPostByCategory(int id)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("ID", id);
+                
+                // Need to check SQL Database: table relationship for query below.
+                var post = _cn.Query<Post>("SELECT * " +
+                                           "FROM Post " +
+                                           "WHERE CategoryId = @ID ", parameters).FirstOrDefault();
+                return post;
+            }
+        }
+
+        public Post GetPostByTag(int id)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("ID", id);
+
+                // Need to check SQL Database: table relationship for query below.
+                var post = _cn.Query<Post>("SELECT * " +
+                                           "FROM Post " +
+                                           "WHERE TagId = @ID ", parameters).FirstOrDefault();
+                return post;
+            }
+        }
+
+        public void Add(Post post)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var parameters = new DynamicParameters();
+
+                // add more parameters here
+                parameters.Add("BlogId", post.BlogId);
+                parameters.Add("PostContent", post.PostContent);
+
+                string query = "INSERT INTO Post (BlogId, PostContent) " +
+                               "VALUES (@BlogId, @PostContent) ";
+
+                _cn.Execute(query, parameters);
+            }
+        }
+
+        public void Delete(int postId)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var param = new DynamicParameters();
+                param.Add("PostId", postId);
+
+                // Need to write a Stored Procedure
+                _cn.Execute("DeletePost", param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void Edit(int postId, Post post)
+        {
+            using (var _cn = new SqlConnection(conn))
+            {
+                var parameters = new DynamicParameters();
+
+
+                // add more parameters here
+                parameters.Add("ID", postId);
+                parameters.Add("PostTitle", post.PostTitle);
+                parameters.Add("PostDate", DateTime.Today);
+
+                string query = "UPDATE Post SET PostTitle=@PostTitle, PostDate=@PostDate" +
+                               "WHERE RequestFormId = @ID";
+                _cn.Execute(query, parameters);
+            }
         }
     }
 }
--- AnatoknightStudios/AnatoknightStudios.UI/Controllers/BlogController.cs
+++ AnatoknightStudios/AnatoknightStudios.UI/Controllers/BlogController.cs
@@ -11,24 +11,81 @@ namespace AnatoknightStudios.UI.Controllers
     public class BlogController : Controller
     {
 
-        private static List<Post> _repo = new List<Post>();
+        //private static List<Post> _repo = new List<Post>();
 
         // GET: Contributor
         public ActionResult Index()
         {
-            return View(_repo);
+            var ops = new BlogOperations();
+            var posts = ops.GetAllPosts();
+
+            return View(posts);
+            //return View(_repo);
         }
-        
-    
+
+        // GET: Create a new post
         public ActionResult Add()
         {
             return View("Add", new Post());
         }
 
+        // POST: Create a new post
         [HttpPost]
-        public ActionResult Add(Post model)
+        public ActionResult Add(Post post)
         {
-            _repo.Add(model);
+            var ops = new BlogOperations();
+            ops.Add(post);
+            //_repo.Add(post);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Details of post
+        public ActionResult Details(int id)
+        {
+            var ops = new BlogOperations();
+            Post post = new Post();
+            post = ops.GetPostById(id);
+            return View(post);
+        }
+
+        // GET: Delete a post
+        public ActionResult Delete(int id)
+        {
+            var ops = new BlogOperations();
+            var post = ops.GetPostById(id);
+
+            return View(post);
+        }
+
+        // POST: Delete a post
+        [HttpPost]
+
+        // Add contributors or users in Roles
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(Post id)
+        {
+            var ops = new BlogOperations();
+            ops.Delete(id.PostId);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var ops = new BlogOperations();
+            var post = ops.GetPostById(id);
+            return View(post);
+        }
+
+        [HttpPost]
+
+        // Add contributors or users in Roles
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(Post post)
+        {
+            var ops = new BlogOperations();
+            ops.Edit(post.PostId, post);
             return RedirectToAction("Index");
         }
     }
