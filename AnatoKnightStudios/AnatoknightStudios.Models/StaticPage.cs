a9679d9ddb8fe5fe1296e06488401baa5976309c
Kirkland Brown <kizzet373@gmail.com>
2016-04-25 16:42:13 -0400

    KKJC-11 - modified models

---
+++ AnatoKnightStudios/AnatoknightStudios.Models/StaticPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnatoknightStudios.Models
+{
+    public class StaticPage
+    {
+        public int PageId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
