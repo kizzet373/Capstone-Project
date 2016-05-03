d866cd67dad8985b7641c21839727cfe3a8d3826
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-05-03 16:09:33 -0400

    KKJC-23 unit testing

---
+++ AnatoknightStudios/AnatoknightStudios.Tests/DataTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnatoknightStudios.Data.Repos;
+using AnatoknightStudios.Models;
+using NUnit.Framework;
+
+namespace AnatoknightStudios.Tests
+{
+    [TestFixture]
+    class DataTests
+    {
+        private BlogRepo _repo = new BlogRepo();
+
+        [TestCase(2, 2)]
+        public void GetPostById(int postId, int expectedPost)
+        {
+            Post samplePost = new Post();
+            samplePost = _repo.GetPostById(postId);
+
+            Assert.AreEqual(expectedPost, samplePost.PostId);
+        }
+    }
+}
