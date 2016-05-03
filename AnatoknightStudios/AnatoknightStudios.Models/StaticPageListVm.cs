58e8216fe45c68f84e986e4b762d7b669b3795f6
Joseph Usner <joe.usner@yahoo.com>
2016-05-03 14:48:18 -0400

    KKJC-18 Static page dropdown working, needs a bit of color

---
+++ AnatoknightStudios/AnatoknightStudios.Models/StaticPageListVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnatoknightStudios.Models
+{
+    public class StaticPageListVm
+    {
+        public List<StaticPage> StaticPages { get; set; } 
+    }
+}
