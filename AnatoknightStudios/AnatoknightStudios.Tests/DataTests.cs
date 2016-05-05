61da3e4cb86f3a69848ce9c8952c712fb674a7fd
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-05-04 21:40:51 -0400

    KKJC-23 added more tests

--- AnatoknightStudios/AnatoknightStudios.Tests/DataTests.cs
+++ AnatoknightStudios/AnatoknightStudios.Tests/DataTests.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AnatoknightStudios.Data;
 using AnatoknightStudios.Data.Repos;
@@ -21,7 +22,8 @@ namespace AnatoknightStudios.Tests
         // Mock Repo
         //private MockRepo _repo = new MockRepo();
 
-        private readonly string _conn = ConfigurationManager.ConnectionStrings["AnatoknightStudiosTests"].ConnectionString;
+        private readonly string _conn =
+            ConfigurationManager.ConnectionStrings["AnatoknightStudiosTests"].ConnectionString;
 
         private string _script;
         private BlogRepo _repo = new BlogRepo();
@@ -31,7 +33,7 @@ namespace AnatoknightStudios.Tests
             //var assembly = Assembly.GetExecutingAssembly();
             var assembly = typeof (DataTests).Assembly; //Gets assembly by class name
             var codebase = new Uri(assembly.CodeBase);
-            var path = codebase.LocalPath.Substring(0, 
+            var path = codebase.LocalPath.Substring(0,
                 codebase.LocalPath.LastIndexOf("\\", StringComparison.Ordinal) + 1);
             return path;
         }
@@ -42,7 +44,7 @@ namespace AnatoknightStudios.Tests
             using (SqlConnection sqlConn = new SqlConnection(_conn))
             {
                 _script = File.ReadAllText(AssemblyLocation() + "\\SeedData.sql");
-                
+
                 SqlCommand cmd = new SqlCommand(_script, sqlConn);
                 sqlConn.Open();
                 cmd.ExecuteNonQuery();
@@ -70,7 +72,6 @@ namespace AnatoknightStudios.Tests
             var expected = 18;
             var actual = target.GetAllPosts().Count;
             Assert.AreEqual(actual, expected);
-
         }
 
         [TestCase(2, 2)]
@@ -79,16 +80,35 @@ namespace AnatoknightStudios.Tests
             Post post = new Post();
             post = _repo.GetPostById(postId);
             Assert.AreEqual(expectedPost, post.PostId);
-
         }
-        
+
         [TestCase(3, 1)]
         public void GetPostByCategory(int categoryId, int expectedId)
         {
             List<Post> post = new List<Post>();
             post = _repo.GetPostsByCategory(categoryId);
             Assert.AreEqual(expectedId, post.Count);
+        }
 
+        [TestCase(2, "An awful Idea")]
+        public void GetPostByPostTitle(int postId, string expectedResult)
+        {
+            var result = _repo.GetPostById(postId).PostTitle;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(1, 17)]
+        public void GetPostCountsByCategory(int categoryId, int expectedResult)
+        {
+            var result = _repo.GetPostsByCategory(categoryId).Count;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(2, 6)]
+        public void GetPostCountsByBlogId(int blogId, int expectedResult)
+        {
+            var result = _repo.GetPostsByBlogId(blogId).Count;
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
